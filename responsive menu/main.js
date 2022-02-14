cafbfed53d3b1804788ef98b0060ccc3a724210b
S.M.Samrat <freefire808709@gmail.com>
2022-02-14 18:59:16 +0600

    practice,responsive menu , transition, transform, carousel etc

---
+++ responsive menu/main.js
@@ -0,0 +1,36 @@
+
+$(function(){
+    $('.nav-list > li').hover(function(e){
+        $(this).find('.nav-dropdown').show(300);
+    //     $('.dropdown').not($(this).siblings()).hide();
+    //    e.stopPropagation();
+
+       
+    });
+
+    $('html').click(function() {
+        $('.nav-dropdown').hide();
+        });
+    
+})(jQuery);
+
+
+// /** code by webdevtrick ( https://webdevtrick.com ) **/
+// (function($) { 
+//     $(function() { 
+//       $('nav ul li a:not(:only-child)').click(function(e) {
+//         $(this).siblings('.nav-dropdown').toggle();
+//         $('.dropdown').not($(this).siblings()).hide();
+//         e.stopPropagation();
+//       });
+//       $('html').click(function() {
+//         $('.nav-dropdown').hide();
+//       });
+//       $('#nav-toggle').click(function() {
+//         $('nav ul').slideToggle();
+//       });
+//       $('#nav-toggle').on('click', function() {
+//         this.classList.toggle('active');
+//       });
+//     }); 
+//   })(jQuery);
